3986565643733810e0ae947f442ccb4f68fb9f22
denn <dennq16@gmail.com>
2025-03-06 17:57:07 +0800

    added features and fixed minor bugs in modal section

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function contact(event) {
     event.preventDefault();
-    const loading = document.querySelector('.modal__overlay--loading')
-    const success = document.querySelector('.modal__overlay--success')
-    loading.classList += " modal__overlay--visible"
-    
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
+
     emailjs
         .sendForm(
             'service_5nufpuf',
@@ -11,12 +11,23 @@ function contact(event) {
             event.target,
             '5A7nE4A9eKeospPJF'
     ).then(() => {
-        loading.classList.remove("modal__overlay--visble")
-        success.classList += " modal__overlay--visible"
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
     }).catch(() => {
-        loading.classList.remove("modal__overlay--visble")
+        loading.classList.remove("modal__overlay--visible");
         alert(
             "The email service is temporarily unavailable. Please contact me directly on dennq16@gmail.com"
-        )
+        );
      })
+}
+
+let isModalOpen = false;
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
 }
